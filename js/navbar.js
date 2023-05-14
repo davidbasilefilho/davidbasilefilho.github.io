4d0d16473eaea37389dd7bc0246ecbdb4e496612
David Basile Filho <davidbasilefilho@gmail.com>
2023-05-14 16:07:54 -0300

    made the navbar work properly

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,12 @@
-$("#navbar-include").load("navbar.html");
+$("#navbar-include").load("navbar.html", () => {
+    if (params.has("p") && params.get("p")) {
+        const page = params.get("p");
+        const navlink = document.getElementById(`${page}-navlink`);
 
-if (params.has("p") && params.get("p")) {
-    const page = params.get("p");
-    const navlink = document.getElementById(page + "-navlink");
-
-    console.log(page);
-    if (navlink !== null) {
-        navlink.classList.add("active");
+        if (navlink !== null) {
+            navlink.classList.add("active");
+        }
+    } else {
+        document.getElementById("home-navlink").classList.add("active");
     }
-} else {
-    document.getElementById("home-navlink").classList.add("active");
-}
+});
